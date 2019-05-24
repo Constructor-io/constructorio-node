8e27386411575b9a33ac3db0d51142653568a512
Zubin Tiku <rubikzube@gmail.com>
2019-05-24 12:31:08 -0700

    Updated tests

--- test/constructorio-test.js
+++ test/constructorio-test.js
@@ -85,116 +85,93 @@ describe('constructorio', () => {
     });
   });
 
-  // describe('add batch', () => {
-  //   it('adds multiple items in a batch', (done) => {
-  //     const constructorio = new Constructorio({
-  //       apiToken: 'apiToken',
-  //       autocompleteKey: 'autocompleteKey',
-  //     });
+  describe('addItemBatch', () => {
+    it('should return nothing when adding multiple items to an autocomplete section', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const data = {
+        items: [
+          createProductItem(),
+          createProductItem(),
+          createProductItem(),
+        ],
+        autocomplete_section: 'Products',
+      };
 
-  //     constructorio.add_batch({
-  //       items: [{ item_name: 'reciprocating saw' }],
-  //       autocomplete_section: 'standard',
-  //     }, (err, response) => {
-  //       assert.equal(err, undefined);
-  //       assert.equal(response, '');
-  //       done();
-  //     });
-  //   });
-  // });
+      constructorio.addItemBatch(data, (err, response) => {
+        expect(err).to.be.undefined;
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+  });
 
-  // describe('add batch with metadata', () => {
-  //   it('adds multiple items in a batch', (done) => {
-  //     const constructorio = new Constructorio({
-  //       apiToken: 'apiToken',
-  //       autocompleteKey: 'autocompleteKey',
-  //     });
+  describe('addOrUpdateItem', () => {
+    it('should return nothing when upserting an item to an autocomplete section', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const data = createProductItem();
+      data.autocomplete_section = 'Products';
 
-  //     constructorio.add_batch({
-  //       items: [
-  //         { item_name: 'reciprocating saw 1', url: 'http://url.com', metadata: { key1: 'value1', key2: 'value2' } },
-  //         { item_name: 'reciprocating saw 2', url: 'http://url.com', metadata: { keyA: 'valueA', keyB: 'valueB' } },
-  //       ],
-  //       autocomplete_section: 'Products',
-  //     }, (err, response) => {
-  //       assert.equal(err, undefined);
-  //       assert.equal(response, '');
-  //       done();
-  //     });
-  //   });
-  // });
+      constructorio.addOrUpdateItem(data, (err, response) => {
+        expect(err).to.be.undefined;
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+  });
 
-  // describe('add or update', () => {
-  //   it('adds/updates an item', (done) => {
-  //     const constructorio = new Constructorio({
-  //       apiToken: 'apiToken',
-  //       autocompleteKey: 'autocompleteKey',
-  //     });
+  describe('addOrUpdateItemBatch', () => {
+    it('should return nothing when upserting multiple items to an autocomplete section', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const data = {
+        items: [
+          createProductItem(),
+          createProductItem(),
+          createProductItem(),
+        ],
+        autocomplete_section: 'Products',
+      };
 
-  //     constructorio.add_or_update({
-  //       item_name: 'power drill',
-  //       autocomplete_section: 'standard',
-  //     }, (err, response) => {
-  //       assert.equal(err, undefined);
-  //       assert.equal(response, '');
-  //       done();
-  //     });
-  //   });
-  // });
+      constructorio.addOrUpdateItemBatch(data, (err, response) => {
+        expect(err).to.be.undefined;
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+  });
 
-  // describe('add or update batch', () => {
-  //   it('adds/updates multiple items in a batch', (done) => {
-  //     const constructorio = new Constructorio({
-  //       apiToken: 'apiToken',
-  //       autocompleteKey: 'autocompleteKey',
-  //     });
+  describe('removeItem', () => {
+    it('should return nothing when removing an item from an autocomplete section', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const data = createProductItem();
+      data.autocomplete_section = 'Products';
 
-  //     constructorio.add_or_update_batch({
-  //       items: [{ item_name: 'reciprocating saw' }],
-  //       autocomplete_section: 'standard',
-  //     }, (err, response) => {
-  //       assert.equal(err, undefined);
-  //       assert.equal(response, '');
-  //       done();
-  //     });
-  //   });
-  // });
+      constructorio.removeItem(data, (err, response) => {
+        expect(err).to.be.undefined;
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+  });
 
-  // describe('remove', () => {
-  //   it('removes an item', (done) => {
-  //     const constructorio = new Constructorio({
-  //       apiToken: 'apiToken',
-  //       autocompleteKey: 'autocompleteKey',
-  //     });
+  describe('removeItemBatch', () => {
+    it('should return nothing when removing multiple items to an autocomplete section', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const data = {
+        items: [
+          createProductItem(),
+          createProductItem(),
+          createProductItem(),
+        ],
+        autocomplete_section: 'Products',
+      };
 
-  //     constructorio.remove({
-  //       item_name: 'power drill',
-  //       autocomplete_section: 'standard',
-  //     }, (err, response) => {
-  //       assert.equal(err, undefined);
-  //       assert.equal(response, '');
-  //       done();
-  //     });
-  //   });
-  // });
-
-  // describe('remove batch', () => {
-  //   it('removes multiple items in a batch', (done) => {
-  //     const constructorio = new Constructorio({
-  //       apiToken: 'apiToken',
-  //       autocompleteKey: 'autocompleteKey',
-  //     });
-
-  //     constructorio.remove_batch({
-  //       items: [{ item_name: 'reciprocating saw' }],
-  //       autocomplete_section: 'standard',
-  //     }, (err, response) => {
-  //       assert.equal(err, undefined);
-  //       assert.equal(response, '');
-  //       done();
-  //     });
-  //   });
-  // });
+      constructorio.removeItemBatch(data, (err, response) => {
+        expect(err).to.be.undefined;
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+  });
 
   // describe('modify', () => {
   //   it('modifies an item', (done) => {
