8b5bd6ea7f512097cee64be4b0e0eef57bf48c7a
Steve Blaurock <sblaurock@gmail.com>
2019-08-07 12:42:50 -0600

    Add validation for `s`, `i`, `us` and `ui` parameters + tests.

--- test/constructorio-autocomplete-test.js
+++ test/constructorio-autocomplete-test.js
@@ -9,6 +9,11 @@ const testConfig = {
 };
 
 describe('ConstructorIO - Autocomplete', () => {
+  const personalizationParameters = {
+    s: 1,
+    i: 'user',
+  };
+
   describe('getAutocompleteResults', () => {
     it('should return autocomplete results when supplying a valid query', (done) => {
       const constructorio = new Constructorio(testConfig);
@@ -16,6 +21,7 @@ describe('ConstructorIO - Autocomplete', () => {
 
       constructorio.getAutocompleteResults({
         query,
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.undefined;
         expect(response).to.be.an('object');
@@ -36,6 +42,7 @@ describe('ConstructorIO - Autocomplete', () => {
 
       constructorio.getAutocompleteResults({
         query,
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.undefined;
         expect(response).to.be.an('object');
@@ -52,6 +59,7 @@ describe('ConstructorIO - Autocomplete', () => {
       constructorio.getAutocompleteResults({
         query: 'drill',
         num_results: 4,
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.undefined;
         expect(response).to.be.an('object');
@@ -68,6 +76,7 @@ describe('ConstructorIO - Autocomplete', () => {
         query: 'drill',
         'num_results_Search Suggestions': 3,
         num_results_Products: 4,
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.undefined;
         expect(response).to.be.an('object');
@@ -85,6 +94,7 @@ describe('ConstructorIO - Autocomplete', () => {
         'num_results_Search Suggestions': 3,
         num_results_Products: 4,
         num_results: 2,
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.undefined;
         expect(response).to.be.an('object');
@@ -100,6 +110,7 @@ describe('ConstructorIO - Autocomplete', () => {
       constructorio.getAutocompleteResults({
         query: 'drill',
         filters: ['xylophone'],
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.undefined;
         expect(response).to.be.an('object');
@@ -110,12 +121,79 @@ describe('ConstructorIO - Autocomplete', () => {
       });
     });
 
+    it('should return autocomplete results when supplying a valid optional ui parameter', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const query = 'Stanley';
+
+      constructorio.getAutocompleteResults({
+        query,
+        ...personalizationParameters,
+        ui: 'testing',
+      }, (err, response) => {
+        expect(err).to.be.undefined;
+        expect(response).to.be.an('object');
+        expect(response.sections).to.have.property('Search Suggestions').that.is.an('array');
+        expect(response.sections).to.have.property('Products').that.is.an('array');
+        done();
+      });
+    });
+
+    it('should return autocomplete results when supplying a valid optional us parameter', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const query = 'Stanley';
+
+      constructorio.getAutocompleteResults({
+        query,
+        ...personalizationParameters,
+        us: ['foo', 'bar'],
+      }, (err, response) => {
+        expect(err).to.be.undefined;
+        expect(response).to.be.an('object');
+        expect(response.sections).to.have.property('Search Suggestions').that.is.an('array');
+        expect(response.sections).to.have.property('Products').that.is.an('array');
+        done();
+      });
+    });
+
+    it('should return autocomplete results when supplying invalid optional ui parameter', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const query = 'Stanley';
+
+      constructorio.getAutocompleteResults({
+        query,
+        ...personalizationParameters,
+        ui: {},
+      }, (err, response) => {
+        expect(err).to.be.an('object');
+        expect(err).to.have.property('message', 'Request could not be completed - `ui` parameter must be a string');
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should return autocomplete results when supplying invalid optional us parameter', (done) => {
+      const constructorio = new Constructorio(testConfig);
+      const query = 'Stanley';
+
+      constructorio.getAutocompleteResults({
+        query,
+        ...personalizationParameters,
+        us: 'failure',
+      }, (err, response) => {
+        expect(err).to.be.an('object');
+        expect(err).to.have.property('message', 'Request could not be completed - `us` parameter must be a list');
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+
     it('should return error when invalid num results parameter is supplied', (done) => {
       const constructorio = new Constructorio(testConfig);
 
       constructorio.getAutocompleteResults({
         query: 'drill',
         num_results: 'abc',
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.an('object');
         expect(err).to.have.property('message', 'num_results must be an integer');
@@ -130,6 +208,7 @@ describe('ConstructorIO - Autocomplete', () => {
       constructorio.getAutocompleteResults({
         query: 'drill',
         num_results_Products: 'abc',
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.an('object');
         expect(err).to.have.property('message', 'num_results_Products must be an integer');
@@ -144,6 +223,7 @@ describe('ConstructorIO - Autocomplete', () => {
       constructorio.getAutocompleteResults({
         query: 'drill',
         filters: 'abc',
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.an('object');
         expect(err).to.have.property('message', 'Failed to parse the provided filters. Please check the syntax and try again');
@@ -158,9 +238,38 @@ describe('ConstructorIO - Autocomplete', () => {
       constructorio.getAutocompleteResults({
         query: 'drill',
         filters: ['abc', 'def'],
+        ...personalizationParameters,
       }, (err, response) => {
         expect(err).to.be.an('object');
         expect(err).to.have.property('message', 'Only single filters are supported in autocomplete, but you seem to have provided more than one.');
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should return error when no personalization parameters are supplied', (done) => {
+      const constructorio = new Constructorio(testConfig);
+
+      constructorio.getAutocompleteResults({
+        query: 'drill',
+      }, (err, response) => {
+        expect(err).to.be.an('object');
+        expect(err).to.have.property('message', 'Request could not be completed - `s` and `i` are required parameters and must be a number and string, respectively');
+        expect(response).to.be.undefined;
+        done();
+      });
+    });
+
+    it('should return error when invalid personalization parameters are supplied', (done) => {
+      const constructorio = new Constructorio(testConfig);
+
+      constructorio.getAutocompleteResults({
+        query: 'drill',
+        s: 'abc',
+        i: 0,
+      }, (err, response) => {
+        expect(err).to.be.an('object');
+        expect(err).to.have.property('message', 'Request could not be completed - `s` and `i` are required parameters and must be a number and string, respectively');
         expect(response).to.be.undefined;
         done();
       });
@@ -172,7 +281,10 @@ describe('ConstructorIO - Autocomplete', () => {
         apiKey: 'bad-key',
       });
 
-      constructorio.getAutocompleteResults({}, (err, response) => {
+      constructorio.getAutocompleteResults({
+        query: 'drill',
+        ...personalizationParameters,
+      }, (err, response) => {
         expect(err).to.be.an('object');
         expect(err).to.have.property('message').to.match(/We have no record of this key./);
         expect(response).to.be.undefined;
