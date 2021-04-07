23fa6013ddf1f4c1a52c2722f001272ce5ec101a
Steve Blaurock <sblaurock@gmail.com>
2021-04-07 12:00:23 -0600

    Remove modules no longer necessary for this application.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,5 @@
 /* eslint-disable no-param-reassign */
 const qs = require('qs');
-const CRC32 = require('crc-32');
-const store = require('./store');
-
-const purchaseEventStorageKey = '_constructorio_purchase_order_ids';
 
 const utils = {
   ourEncodeURIComponent: (str) => {
@@ -46,97 +42,7 @@ const utils = {
     throw error;
   }),
 
-  hasWindow: () => typeof window !== 'undefined',
-
-  addEventListener: (eventType, callback, useCapture) => {
-    if (utils.hasWindow()) {
-      window.addEventListener(eventType, callback, useCapture);
-    }
-  },
-
-  removeEventListener: (eventType, callback, useCapture) => {
-    if (utils.hasWindow()) {
-      window.removeEventListener(eventType, callback, useCapture);
-    }
-  },
-
-  getNavigator: () => {
-    if (utils.hasWindow()) {
-      return window.navigator;
-    }
-
-    return {
-      userAgent: '',
-      webdriver: false,
-    };
-  },
-
   isNil: value => value == null,
-
-  getWindowLocation: () => {
-    if (utils.hasWindow()) {
-      return window.location;
-    }
-
-    return {};
-  },
-
-  dispatchEvent: (event) => {
-    if (utils.hasWindow()) {
-      window.dispatchEvent(event);
-    }
-  },
-
-  createCustomEvent: (eventName, detail) => {
-    if (utils.hasWindow()) {
-      try {
-        return new window.CustomEvent(eventName, { detail });
-      } catch (e) {
-        const evt = document.createEvent('CustomEvent');
-
-        evt.initCustomEvent(eventName, false, false, detail);
-
-        return evt;
-      }
-    }
-
-    return null;
-  },
-
-  hasOrderIdRecord(orderId) {
-    const purchaseEventStorage = JSON.parse(store.session.get(purchaseEventStorageKey));
-    const orderIdHash = CRC32.str(orderId.toString());
-
-    if (purchaseEventStorage && purchaseEventStorage[orderIdHash]) {
-      return true;
-    }
-
-    return null;
-  },
-
-  addOrderIdRecord(orderId) {
-    let purchaseEventStorage = JSON.parse(store.session.get(purchaseEventStorageKey));
-    const orderIdHash = CRC32.str(orderId.toString());
-
-    if (purchaseEventStorage) {
-
-      // If the order already exists, do nothing
-      if (purchaseEventStorage[orderIdHash]) {
-        return;
-      }
-
-      purchaseEventStorage[orderIdHash] = true;
-    } else {
-
-      // Create a new object map for the order ids
-      purchaseEventStorage = {
-        [orderIdHash]: true,
-      };
-    }
-
-    // Push the order id map into session storage
-    store.session.set(purchaseEventStorageKey, JSON.stringify(purchaseEventStorage));
-  },
 };
 
 module.exports = utils;
