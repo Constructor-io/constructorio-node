d594262495741528f751e9acaa67654159dfec0f
Daniel Tillero <danieltillero67@gmail.com>
2021-06-01 19:26:25 -0400

    [CSL 341]: Port getBrowseResultsByItemIds (#31)
    * implement getBrowseResultsByItemIds
    
    * lint
    
    * Add headers to fetch for getBrowseResultsByItemIds
    
    * Refactor createBrowseUrl and getBrowseResultsByItemIds to use createQueryParams as helper
    
    * Remove event dispatcher
    
    * remove .only from test file
    
    * lint
    
    * lint
    
    * Lint changes and user parameters for documentation
    
    * Consistency updates.
    
    * Rename getBrowseResultsByItemIds to getBrowseResultsForItemIds
    
    Co-authored-by: Enes Kutay SEZEN <eneskutaysezen@gmail.com>
    Co-authored-by: Steve Blaurock <sblaurock@gmail.com>

--- src/modules/browse.js
+++ src/modules/browse.js
@@ -1,15 +1,13 @@
-/* eslint-disable object-curly-newline, no-underscore-dangle */
+/* eslint-disable object-curly-newline, no-underscore-dangle, max-params */
 const qs = require('qs');
 const nodeFetch = require('node-fetch');
 const helpers = require('../utils/helpers');
 
-// Create URL from supplied filter name, value and parameters
-// eslint-disable-next-line max-params
-function createBrowseUrl(filterName, filterValue, parameters, userParameters, options) {
+// Create query params from parameters and options
+function createQueryParams(parameters, userParameters, options) {
   const {
     apiKey,
     version,
-    serviceUrl,
   } = options;
   const {
     sessionId,
@@ -23,33 +21,6 @@ function createBrowseUrl(filterName, filterValue, parameters, userParameters, op
   queryParams.key = apiKey;
   queryParams.i = clientId;
   queryParams.s = sessionId;
-
-  // Validate filter name is provided
-  if (!filterName || typeof filterName !== 'string') {
-    throw new Error('filterName is a required parameter of type string');
-  }
-
-  // Validate filter value is provided
-  if (!filterValue || typeof filterValue !== 'string') {
-    throw new Error('filterValue is a required parameter of type string');
-  }
-
-  // Pull test cells from options
-  if (testCells) {
-    Object.keys(testCells).forEach((testCellKey) => {
-      queryParams[`ef-${testCellKey}`] = testCells[testCellKey];
-    });
-  }
-
-  // Pull user segments from options
-  if (segments && segments.length) {
-    queryParams.us = segments;
-  }
-
-  // Pull user id from options
-  if (userId) {
-    queryParams.ui = userId;
-  }
 
   if (parameters) {
     const { page, resultsPerPage, filters, sortBy, sortOrder, section, fmtOptions } = parameters;
@@ -89,12 +60,62 @@ function createBrowseUrl(filterName, filterValue, parameters, userParameters, op
     }
   }
 
+  // Pull test cells from options
+  if (testCells) {
+    Object.keys(testCells).forEach((testCellKey) => {
+      queryParams[`ef-${testCellKey}`] = testCells[testCellKey];
+    });
+  }
+
+  // Pull user segments from options
+  if (segments && segments.length) {
+    queryParams.us = segments;
+  }
+
+  // Pull user id from options
+  if (userId) {
+    queryParams.ui = userId;
+  }
+
   queryParams._dt = Date.now();
   queryParams = helpers.cleanParams(queryParams);
 
+  return queryParams;
+}
+
+// Create URL from supplied filter name, value and parameters
+function createBrowseUrlFromFilter(filterName, filterValue, parameters, userParameters, options) {
+  const { serviceUrl } = options;
+
+  // Validate filter name is provided
+  if (!filterName || typeof filterName !== 'string') {
+    throw new Error('filterName is a required parameter of type string');
+  }
+
+  // Validate filter value is provided
+  if (!filterValue || typeof filterValue !== 'string') {
+    throw new Error('filterValue is a required parameter of type string');
+  }
+
+  const queryParams = createQueryParams(parameters, userParameters, options);
   const queryString = qs.stringify(queryParams, { indices: false });
 
   return `${serviceUrl}/browse/${encodeURIComponent(filterName)}/${encodeURIComponent(filterValue)}?${queryString}`;
+}
+
+// Create URL from supplied ID's
+function createBrowseUrlFromIDs(itemIds, parameters, userParameters, options) {
+  const { serviceUrl } = options;
+
+  // Validate item ID's are provided
+  if (!itemIds || !(itemIds instanceof Array) || !itemIds.length) {
+    throw new Error('itemIds is a required parameter of type array');
+  }
+
+  const queryParams = { ...createQueryParams(parameters, userParameters, options), ids: itemIds };
+  const queryString = qs.stringify(queryParams, { indices: false });
+
+  return `${serviceUrl}/browse/items?${queryString}`;
 }
 
 /**
@@ -139,7 +160,7 @@ class Browse {
     const headers = {};
 
     try {
-      requestUrl = createBrowseUrl(filterName, filterValue, parameters, userParameters, this.options);
+      requestUrl = createBrowseUrlFromFilter(filterName, filterValue, parameters, userParameters, this.options);
     } catch (e) {
       return Promise.reject(e);
     }
@@ -185,6 +206,78 @@ class Browse {
 
       throw new Error('getBrowseResults response data is malformed');
     });
+  }
+
+  /**
+   * Retrieve browse results from API using item ID's
+   *
+   * @function getBrowseResultsForItemIds
+   * @param {string[]} itemIds - Item ID's of results to fetch
+   * @param {object} [parameters] - Additional parameters to refine result set
+   * @param {number} [parameters.page] - The page number of the results
+   * @param {number} [parameters.resultsPerPage] - The number of results per page to return
+   * @param {object} [parameters.filters] - Filters used to refine results
+   * @param {string} [parameters.sortBy='relevance'] - The sort method for results
+   * @param {string} [parameters.sortOrder='descending'] - The sort order for results
+   * @param {object} [parameters.fmtOptions] - The format options used to refine result groups
+   * @param {object} [userParameters] - Parameters relevant to the user request
+   * @param {number} [userParameters.sessionId] - Session ID, utilized to personalize results
+   * @param {number} [userParameters.clientId] - Client ID, utilized to personalize results
+   * @param {object} [userParameters.userId] - User ID, utilized to personalize results
+   * @param {string} [userParameters.segments] - User segments
+   * @param {string} [userParameters.testCells] - User test cells
+   * @param {string} [userParameters.userIp] - Origin user IP, from client
+   * @param {string} [userParameters.userAgent] - Origin user agent, from client
+   * @returns {Promise}
+   * @see https://docs.constructor.io/rest_api/browse/items/
+  */
+  getBrowseResultsForItemIds(itemIds, parameters = {}, userParameters = {}) {
+    let requestUrl;
+    const fetch = (this.options && this.options.fetch) || nodeFetch;
+    const headers = {};
+
+    try {
+      requestUrl = createBrowseUrlFromIDs(itemIds, parameters, userParameters, this.options);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+
+    // Append security token as 'x-cnstrc-token' if available
+    if (this.options.securityToken && typeof this.options.securityToken === 'string') {
+      headers['x-cnstrc-token'] = this.options.securityToken;
+    }
+
+    // Append user IP as 'X-Forwarded-For' if available
+    if (userParameters.userIp && typeof userParameters.userIp === 'string') {
+      headers['X-Forwarded-For'] = userParameters.userIp;
+    }
+
+    // Append user agent as 'User-Agent' if available
+    if (userParameters.userAgent && typeof userParameters.userAgent === 'string') {
+      headers['User-Agent'] = userParameters.userAgent;
+    }
+
+    return fetch(requestUrl, { headers })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        return helpers.throwHttpErrorFromResponse(new Error(), response);
+      })
+      .then((json) => {
+        if (json.response && json.response.results) {
+          if (json.result_id) {
+            // Append `result_id` to each result item
+            json.response.results.forEach((result) => {
+              // eslint-disable-next-line no-param-reassign
+              result.result_id = json.result_id;
+            });
+          }
+          return json;
+        }
+        throw new Error('getBrowseResultsForItemIds response data is malformed');
+      });
   }
 }
 
