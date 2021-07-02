a4a68a19c9475482a3af8069da3af793e9105581
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2021-07-02 17:38:49 -0400

    [CSL-407] Add browse groups (#34)
    * Add browse groups endpoint
    
    * Add tests
    
    * Styling

--- src/modules/browse.js
+++ src/modules/browse.js
@@ -118,6 +118,28 @@ function createBrowseUrlFromIDs(itemIds, parameters, userParameters, options) {
   return `${serviceUrl}/browse/items?${queryString}`;
 }
 
+// Create request headers using supplied options and user parameters
+function createHeaders(options, userParameters) {
+  const headers = {};
+
+  // Append security token as 'x-cnstrc-token' if available
+  if (options.securityToken && typeof options.securityToken === 'string') {
+    headers['x-cnstrc-token'] = options.securityToken;
+  }
+
+  // Append user IP as 'X-Forwarded-For' if available
+  if (userParameters.userIp && typeof userParameters.userIp === 'string') {
+    headers['X-Forwarded-For'] = userParameters.userIp;
+  }
+
+  // Append user agent as 'User-Agent' if available
+  if (userParameters.userAgent && typeof userParameters.userAgent === 'string') {
+    headers['User-Agent'] = userParameters.userAgent;
+  }
+
+  return headers;
+}
+
 /**
  * Interface to browse related API calls
  *
@@ -157,27 +179,12 @@ class Browse {
   getBrowseResults(filterName, filterValue, parameters = {}, userParameters = {}) {
     let requestUrl;
     const fetch = (this.options && this.options.fetch) || nodeFetch;
-    const headers = {};
+    const headers = createHeaders(this.options, userParameters);
 
     try {
       requestUrl = createBrowseUrlFromFilter(filterName, filterValue, parameters, userParameters, this.options);
     } catch (e) {
       return Promise.reject(e);
-    }
-
-    // Append security token as 'x-cnstrc-token' if available
-    if (this.options.securityToken && typeof this.options.securityToken === 'string') {
-      headers['x-cnstrc-token'] = this.options.securityToken;
-    }
-
-    // Append user IP as 'X-Forwarded-For' if available
-    if (userParameters.userIp && typeof userParameters.userIp === 'string') {
-      headers['X-Forwarded-For'] = userParameters.userIp;
-    }
-
-    // Append user agent as 'User-Agent' if available
-    if (userParameters.userAgent && typeof userParameters.userAgent === 'string') {
-      headers['User-Agent'] = userParameters.userAgent;
     }
 
     return fetch(requestUrl, { headers }).then((response) => {
@@ -234,27 +241,12 @@ class Browse {
   getBrowseResultsForItemIds(itemIds, parameters = {}, userParameters = {}) {
     let requestUrl;
     const fetch = (this.options && this.options.fetch) || nodeFetch;
-    const headers = {};
+    const headers = createHeaders(this.options, userParameters);
 
     try {
       requestUrl = createBrowseUrlFromIDs(itemIds, parameters, userParameters, this.options);
     } catch (e) {
       return Promise.reject(e);
-    }
-
-    // Append security token as 'x-cnstrc-token' if available
-    if (this.options.securityToken && typeof this.options.securityToken === 'string') {
-      headers['x-cnstrc-token'] = this.options.securityToken;
-    }
-
-    // Append user IP as 'X-Forwarded-For' if available
-    if (userParameters.userIp && typeof userParameters.userIp === 'string') {
-      headers['X-Forwarded-For'] = userParameters.userIp;
-    }
-
-    // Append user agent as 'User-Agent' if available
-    if (userParameters.userAgent && typeof userParameters.userAgent === 'string') {
-      headers['User-Agent'] = userParameters.userAgent;
     }
 
     return fetch(requestUrl, { headers })
@@ -278,6 +270,43 @@ class Browse {
         }
         throw new Error('getBrowseResultsForItemIds response data is malformed');
       });
+  }
+
+  /**
+   * Retrieve browse groups from API
+   *
+   * @function getBrowseGroups
+   * @param {object} [parameters.filters] - Filters used to refine results
+   * @param {object} [parameters.fmtOptions] - The format options used to refine result groups
+   * @param {object} [userParameters] - Parameters relevant to the user request
+   * @param {number} [userParameters.sessionId] - Session ID, utilized to personalize results
+   * @param {number} [userParameters.clientId] - Client ID, utilized to personalize results
+   * @param {object} [userParameters.userId] - User ID, utilized to personalize results
+   * @param {string} [userParameters.segments] - User segments
+   * @param {string} [userParameters.testCells] - User test cells
+   * @param {string} [userParameters.userIp] - Origin user IP, from client
+   * @param {string} [userParameters.userAgent] - Origin user agent, from client
+   * @returns {Promise}
+   * @see https://docs.constructor.io/rest-api.html#browse-groups
+   */
+  getBrowseGroups(parameters = {}, userParameters = {}) {
+    const fetch = (this.options && this.options.fetch) || nodeFetch;
+    const headers = createHeaders(this.options, userParameters);
+    const { serviceUrl } = this.options;
+    const queryParams = createQueryParams(parameters, userParameters, this.options);
+
+    delete queryParams._dt;
+
+    const queryString = qs.stringify(queryParams, { indices: false });
+    const requestUrl = `${serviceUrl}/browse/groups?${queryString}`;
+
+    return fetch(requestUrl, { headers }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return helpers.throwHttpErrorFromResponse(new Error(), response);
+    });
   }
 }
 
