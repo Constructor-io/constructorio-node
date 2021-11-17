aecd18ebd904a0d30974a8fcd9c76fe5f1b4ee6d
Jimmy Li <jimmyli921@gmail.com>
2021-11-17 12:10:39 -0800

    [CSL 724] Update parameter details for trackConversion/trackPurchase (#75)
    * Remove trailing slashes from servive url + test
    
    * Lint fixes
    
    * Update com
    ments for trackConversion/trackPurchase
    
    * Add period for consistency.
    
    * Minor update
    
    * Lint changes
    
    * Update com
    ments for trackConversion/trackPurchase
    
    Co-authored-by: Steve Blaurock <sblaurock@gmail.com>

--- src/modules/tracker.js
+++ src/modules/tracker.js
@@ -602,7 +602,7 @@ class Tracker {
    * @param {string} [term] - Search results query term that led to conversion event
    * @param {object} parameters - Additional parameters to be sent with request
    * @param {string} parameters.item_id - Product item unique identifier
-   * @param {number} [parameters.revenue] - Revenue (price) of product item
+   * @param {number} [parameters.revenue] - Sale price if available, otherwise the regular (retail) price of item
    * @param {string} [parameters.item_name] - Product item name
    * @param {string} [parameters.variation_id] - Product item variation unique identifier
    * @param {string} [parameters.type='add_to_cart'] - Conversion type
@@ -732,7 +732,7 @@ class Tracker {
    * @function trackPurchase
    * @param {object} parameters - Additional parameters to be sent with request
    * @param {object[]} parameters.items - List of product item objects
-   * @param {number} parameters.revenue - Revenue
+   * @param {number} parameters.revenue - The subtotal (excluding taxes, shipping, etc.) of the entire order
    * @param {string} [parameters.order_id] - Unique order identifier
    * @param {string} [parameters.section] - Index section
    * @param {object} userParameters - Parameters relevant to the user request
