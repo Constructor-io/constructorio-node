5ff4941045a9708a8d2ea16b80a412d01491991b
Jimmy Li <jimmyli921@gmail.com>
2022-01-03 14:52:18 -0800

    Add support for tasks module

---
+++ spec/src/modules/tasks.js
@@ -0,0 +1,179 @@
+/* eslint-disable no-unused-expressions, import/no-unresolved, no-restricted-syntax, max-nested-callbacks */
+const dotenv = require('dotenv');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+const nodeFetch = require('node-fetch').default;
+const cloneDeep = require('lodash.clonedeep');
+const ConstructorIO = require('../../../test/constructorio'); // eslint-disable-line import/extensions
+const helpers = require('../../mocha.helpers');
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+dotenv.config();
+
+const sendTimeout = 300;
+const testApiKey = process.env.TEST_API_KEY;
+const testApiToken = process.env.TEST_API_TOKEN;
+const validOptions = {
+  apiKey: testApiKey,
+  apiToken: testApiToken,
+};
+
+describe('ConstructorIO - Tasks', function ConstructorIOTasks() {
+  // Ensure Mocha doesn't time out waiting for operation to complete
+  this.timeout(10000);
+
+  const clientVersion = 'cio-mocha';
+  let fetchSpy;
+  let taskId;
+
+  before(async () => {
+    const { catalog } = new ConstructorIO({
+      ...validOptions,
+    });
+
+    // Grab items file from Integration Examples repo and upload
+    const itemsResponse = await nodeFetch('https://raw.githubusercontent.com/Constructor-io/integration-examples/main/catalog/items.csv');
+    const itemsBuffer = await itemsResponse.buffer();
+
+    const data = {
+      items: itemsBuffer,
+      section: 'Products',
+    };
+
+    // Save task id of uploaded catalog for the following tests below
+    await catalog.replaceCatalog(data).then((res) => {
+      taskId = res.task_id;
+    });
+  });
+
+  beforeEach(() => {
+    global.CLIENT_VERSION = clientVersion;
+    fetchSpy = sinon.spy(nodeFetch);
+  });
+
+  afterEach((done) => {
+    delete global.CLIENT_VERSIONS;
+
+    fetchSpy = null;
+
+    setTimeout(done, sendTimeout);
+  });
+
+  describe('getAllTasks', () => {
+    it('Should retrieve a list of tasks', (done) => {
+      const { tasks } = new ConstructorIO({
+        ...validOptions,
+        fetch: fetchSpy,
+      });
+
+      tasks.getAllTasks().then((res) => {
+        const requestedUrlParams = helpers.extractUrlParamsFromFetch(fetchSpy);
+
+        expect(fetchSpy).to.have.been.called;
+        expect(res.total_count).to.be.gte(1);
+        expect(res.tasks[0].id).to.eq(taskId);
+        expect(requestedUrlParams).to.have.property('key');
+        done();
+      });
+    });
+
+    it('Should return error when retrieving a list of tasks with an invalid API key', () => {
+      const invalidOptions = cloneDeep(validOptions);
+      invalidOptions.apiKey = 'notanapikey';
+
+      const { tasks } = new ConstructorIO({
+        ...invalidOptions,
+        fetch: fetchSpy,
+      });
+
+      return expect(tasks.getAllTasks()).to.eventually.be.rejected;
+    });
+
+    it('Should return error when retrieving a list of tasks with an invalid API token', () => {
+      const invalidOptions = cloneDeep(validOptions);
+      invalidOptions.apiToken = 'notanapitoken';
+
+      const { tasks } = new ConstructorIO({
+        ...invalidOptions,
+        fetch: fetchSpy,
+      });
+
+      return expect(tasks.getAllTasks()).to.eventually.be.rejected;
+    });
+
+    it('Should be rejected when network request timeout is provided and reached', () => {
+      const { tasks } = new ConstructorIO(validOptions);
+
+      return expect(tasks.getAllTasks({}, { timeout: 10 })).to.eventually.be.rejectedWith('The user aborted a request.');
+    });
+
+    it('Should be rejected when global network request timeout is provided and reached', () => {
+      const { tasks } = new ConstructorIO({
+        ...validOptions,
+        networkParameters: { timeout: 20 },
+      });
+
+      return expect(tasks.getAllTasks()).to.eventually.be.rejectedWith('The user aborted a request.');
+    });
+  });
+
+  describe('getTask', () => {
+    it('Should retrieve a task given a specific id', (done) => {
+      const { tasks } = new ConstructorIO({
+        ...validOptions,
+        fetch: fetchSpy,
+      });
+
+      tasks.getTask({ id: taskId }).then((res) => {
+        const requestedUrlParams = helpers.extractUrlParamsFromFetch(fetchSpy);
+
+        expect(fetchSpy).to.have.been.called;
+        expect(res.id).to.eq(taskId);
+        expect(requestedUrlParams).to.have.property('key');
+        done();
+      });
+    });
+
+    it('Should return error when retrieving a task given a specific id with an invalid API key', () => {
+      const invalidOptions = cloneDeep(validOptions);
+      invalidOptions.apiKey = 'notanapikey';
+
+      const { tasks } = new ConstructorIO({
+        ...invalidOptions,
+        fetch: fetchSpy,
+      });
+
+      return expect(tasks.getTask()).to.eventually.be.rejected;
+    });
+
+    it('Should return error when retrieving a task given a specific id with an invalid API token', () => {
+      const invalidOptions = cloneDeep(validOptions);
+      invalidOptions.apiToken = 'notanapitoken';
+
+      const { tasks } = new ConstructorIO({
+        ...invalidOptions,
+        fetch: fetchSpy,
+      });
+
+      return expect(tasks.getTask()).to.eventually.be.rejected;
+    });
+
+    it('Should be rejected when network request timeout is provided and reached', () => {
+      const { tasks } = new ConstructorIO(validOptions);
+
+      return expect(tasks.getTask({}, { timeout: 10 })).to.eventually.be.rejectedWith('The user aborted a request.');
+    });
+
+    it('Should be rejected when global network request timeout is provided and reached', () => {
+      const { tasks } = new ConstructorIO({
+        ...validOptions,
+        networkParameters: { timeout: 20 },
+      });
+
+      return expect(tasks.getTask()).to.eventually.be.rejectedWith('The user aborted a request.');
+    });
+  });
+});
---
+++ src/modules/tasks.js
@@ -0,0 +1,141 @@
+/* eslint-disable object-curly-newline, no-underscore-dangle, max-len */
+const qs = require('qs');
+const nodeFetch = require('node-fetch').default;
+const { AbortController } = require('node-abort-controller');
+const helpers = require('../utils/helpers');
+
+// Create URL from supplied path and options
+function createTaskUrl(path, options, additionalQueryParams = {}, apiVersion = 'v1') {
+  const {
+    apiKey,
+    serviceUrl,
+  } = options;
+  let queryParams = {
+    ...additionalQueryParams,
+  };
+
+  // Validate path is provided
+  if (!path || typeof path !== 'string') {
+    throw new Error('path is a required parameter of type string');
+  }
+
+  queryParams.key = apiKey;
+  queryParams = helpers.cleanParams(queryParams);
+
+  const queryString = qs.stringify(queryParams, { indices: false });
+
+  return `${serviceUrl}/${encodeURIComponent(apiVersion)}/${encodeURIComponent(path)}?${queryString}`;
+}
+
+/**
+ * Interface to task related API calls
+ *
+ * @module tasks
+ * @inner
+ * @returns {object}
+ */
+class Tasks {
+  constructor(options) {
+    this.options = options || {};
+  }
+
+  /**
+   * Retrieve all tasks from index
+   *
+   * @function getAllTasks
+   * @param {object} parameters - Additional parameters for task details
+   * @param {number} [parameters.num_results_per_page] - The number of tasks to return. Defaults to 20. Maximum value 100
+   * @param {number} [parameters.page] - The page of results to return. Defaults to 1
+   * @param {object} [networkParameters] - Parameters relevant to the network request
+   * @param {number} [networkParameters.timeout] - Request timeout (in milliseconds)
+   * @returns {Promise}
+   * @see https://docs.constructor.io/rest_api/tasks/#retrieve-all-tasks
+   */
+  getAllTasks(parameters = {}, networkParameters = {}) {
+    const queryParams = {};
+    let requestUrl;
+    const fetch = (this.options && this.options.fetch) || nodeFetch;
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    if (parameters) {
+      const { num_results_per_page: numResultsPerPage, page } = parameters;
+
+      // Pull number of results per page from parameters
+      if (numResultsPerPage) {
+        queryParams.num_results_per_page = numResultsPerPage;
+      }
+
+      // Pull page from parameters
+      if (page) {
+        queryParams.page = page;
+      }
+    }
+
+    try {
+      requestUrl = createTaskUrl('tasks', this.options, queryParams);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+
+    helpers.applyNetworkTimeout(this.options, networkParameters, controller);
+
+    return fetch(requestUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        ...helpers.createAuthHeader(this.options),
+      },
+      signal,
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return helpers.throwHttpErrorFromResponse(new Error(), response);
+    }).then((json) => json);
+  }
+
+  /**
+   * Retrieve task given a specific id
+   *
+   * @function getTask
+   * @param {object} parameters - Additional parameters for task details
+   * @param {string} parameters.id - The ID of the task you'd like to retrieve
+   * @param {object} [networkParameters] - Parameters relevant to the network request
+   * @param {number} [networkParameters.timeout] - Request timeout (in milliseconds)
+   * @returns {Promise}
+   * @see https://docs.constructor.io/rest_api/tasks/#retrieve-all-tasks
+   */
+  getTask(parameters = {}, networkParameters = {}) {
+    let requestUrl;
+    const fetch = (this.options && this.options.fetch) || nodeFetch;
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    try {
+      requestUrl = createTaskUrl(`tasks/${parameters.id}`, this.options);
+    } catch (e) {
+      return Promise.reject(e);
+    }
+
+    helpers.applyNetworkTimeout(this.options, networkParameters, controller);
+
+    return fetch(requestUrl, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        ...helpers.createAuthHeader(this.options),
+      },
+      signal,
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+
+      return helpers.throwHttpErrorFromResponse(new Error(), response);
+    }).then((json) => json);
+  }
+}
+
+module.exports = Tasks;
