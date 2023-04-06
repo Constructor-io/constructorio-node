50ab2a72bdec224db8bb5f02fdfc7a881359bebc
mocca102 <islaam.agamy@gmail.com>
2023-04-06 07:50:22 +0200

    add getRecommendationPods type

--- src/types/tests/recommedations.test-d.ts
+++ src/types/tests/recommedations.test-d.ts
@@ -1,4 +1,5 @@
 import { expectAssignable } from 'tsd';
+import { RecommendationPodsResponse } from '..';
 import { RecommendationsResponse } from '../recommendations.d';
 
 expectAssignable<RecommendationsResponse>({
@@ -42,4 +43,32 @@ expectAssignable<RecommendationsResponse>({
     },
   },
   result_id: '618c2aa1-b851-451f-b228-61d02bf7e3c5',
+});
+
+expectAssignable<RecommendationPodsResponse>({
+  pods: [
+    {
+      strategy: {
+        id: 'recently_viewed_items', display_name: 'Recently Viewed',
+      },
+      id: 'recently-viewed',
+      display_name: 'Recently Viewed',
+      name: 'Recently viewed',
+      created_at: '2023-03-24T20:00:12',
+      updated_at: '2023-03-24T20:00:12',
+      metadata_json: [],
+    },
+    {
+      strategy: {
+        id: 'recently_viewed_items', display_name: 'Recently Viewed',
+      },
+      id: 'you-may-also-like',
+      display_name: 'You May Also Like',
+      name: 'You may also like',
+      created_at: '2023-03-23T15:25:59',
+      updated_at: '2023-03-23T15:32:48',
+      metadata_json: [],
+    },
+  ],
+  total_count: 2,
 });
