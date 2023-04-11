346022120e2410d7df90f25be57f9956a4df999f
Jimmy Li <jimmyli921@gmail.com>
2023-04-11 12:37:24 -0700

    Minor change to messaging

--- scripts/use-jsdocs-salty.sh
+++ scripts/use-jsdocs-salty.sh
@@ -5,5 +5,5 @@ then
   echo "Node module Minami found. Replacing taffydb with salty in publish.js"
   sed -i '' 's/require("taffydb")/require("@jsdoc\/salty")/g' node_modules/minami/publish.js
 else
-  echo "Node module Minami not found."
+  echo "Node module Minami not found. Please make sure to run docs command from the root directory"
 fi
