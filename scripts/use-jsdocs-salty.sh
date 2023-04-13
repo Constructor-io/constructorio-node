dbb3894cd15f187f5c8bde31adb10757540414ce
Jimmy Li <jimmyli921@gmail.com>
2023-04-13 14:59:49 -0700

    Replace minami with docdash

--- scripts/use-jsdocs-salty.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -e node_modules/minami/publish.js ]
-then
-  echo "Node module Minami found. Replacing taffydb with salty in publish.js"
-  sed -i '' 's/require("taffydb")/require("@jsdoc\/salty")/g' node_modules/minami/publish.js
-else
-  echo "Node module Minami not found. Please make sure to run docs command from the root directory"
-fi
