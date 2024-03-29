e134a682f07fc4e6bad57486c7496fce42837bbc
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2024-03-29 13:27:34 -0400

    Add test

--- spec/src/constructorio.js
+++ spec/src/constructorio.js
@@ -5,7 +5,7 @@ const ConstructorIO = require('../../test/constructorio'); // eslint-disable-lin
 const validApiKey = 'testing';
 const validOptions = { apiKey: validApiKey };
 
-describe('ConstructorIO', () => {
+describe.only('ConstructorIO', () => {
   beforeEach(() => {
     global.CLIENT_VERSION = 'cio-mocha';
   });
@@ -51,6 +51,17 @@ describe('ConstructorIO', () => {
     expect(instance.options).to.have.property('version').to.equal(version);
     expect(instance.options).to.have.property('securityToken').to.equal(securityToken);
     expect(instance.options).to.have.property('networkParameters').to.equal(networkParameters);
+  });
+
+  it('Should return an instance with correct serviceUrl when a http serviceUrl is passed', () => {
+    const serviceUrl = 'http://constructor.io';
+    const instance = new ConstructorIO({
+      ...validOptions,
+      serviceUrl,
+    });
+
+    expect(instance).to.be.an('object');
+    expect(instance.options).to.have.property('serviceUrl').to.equal('https://constructor.io');
   });
 
   it('Should remove any trailing slashes from the serviceUrl', () => {
